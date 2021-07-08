5cbef0854b2aeb6886e454b995796b156a3f3319
Damish <damishnisal100@gmail.com>
2021-07-08 17:00:53 +0530

    bottom navigation bar

---
+++ lib/screens/home_page_top_products.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePageTopProducts extends StatefulWidget {
+  const HomePageTopProducts({Key? key}) : super(key: key);
+
+  @override
+  _HomePageTopProductsState createState() => _HomePageTopProductsState();
+}
+
+class _HomePageTopProductsState extends State<HomePageTopProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'home view text',
+      ),
+    );
+  }
+}
