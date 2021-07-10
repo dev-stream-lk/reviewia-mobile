9e56ce1aa7f13619719209d7369cd1bdfe9715ea
Damish <damishnisal100@gmail.com>
2021-07-10 15:48:32 +0530

    added validation class

--- lib/constrains/validation.dart
+++ lib/constrains/validation.dart
@@ -1,3 +1,5 @@
+import 'package:reviewia/screens/register_page.dart';
+
 class Validation {
 
   String? validateUserName(value){
@@ -17,6 +19,57 @@ class Validation {
       return null;
     }
   }
+
+  String? validateName(value){
+    // print((value.toString().contains(new RegExp(r'[^a-zA-Z]')).toString()));
+    if(value!.isEmpty){
+      return "Required Field";
+    }else if(value.toString().contains(new RegExp(r'[0-9]'))){
+      return "Cannot Be contains Numbers";
+    }else if((value.toString().contains(new RegExp(r'[^a-zA-Z]')))){
+      return "Cannot Be Speacial Charcters";
+    }else{
+      return null;
+    }
+  }
+
+  String? validateEmail(value){
+    // print((value.toString().contains(new RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))).toString());
+    if(value!.isEmpty){
+      return "Required Field";
+    }else if(!(value.toString().contains(new RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")))){
+      return "Not a valid type of Email";
+    }else{
+      return null;
+    }
+  }
+
+  String? validPasswordReg(value){
+    String  pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    String upperCase = r'(?=.*[A-Z])';
+    String lowerCase = r'(?=.*[a-z])';
+    String number = r'(?=.*?[0-9])';
+    String sc = r'(?=.*?[!@#\$&*~])';
+    print(value.toString().contains(new RegExp(pattern)).toString());
+    if(value!.isEmpty){
+      return "Required Field";
+    }else if(!(value.toString().contains(new RegExp(upperCase)))){
+      return "include Minimum 1 upper case";
+    }else if(!(value.toString().contains(new RegExp(lowerCase)))){
+      return "include Minimum 1 lower case";
+    }else if(!(value.toString().contains(new RegExp(number)))){
+      return "include Minimum 1 Number";
+    }else if(!(value.toString().contains(new RegExp(sc)))){
+      return "include Minimum 1 special character";
+    }
+    else if(value.toString().length<8){
+      return "Minimum length must be 8";
+    }else{
+      return null;
+    }
+  }
+
+
 
 
 
