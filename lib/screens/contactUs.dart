24eefdb8312af91623ee51db3222e3ed77711b63
Vikum-Kule <vikumkulatunga@gmail.com>
2021-08-19 11:51:00 +0530

    contact us changed

--- lib/screens/contactUs.dart
+++ lib/screens/contactUs.dart
@@ -18,7 +18,7 @@ class _ContactUsState extends State<ContactUs> {
       appBar: AppBar(
         backgroundColor: Kcolor,
         title: Text(
-          "Contact Us",
+          "Contact You",
           style: KappTitle,
         ),
       ),
