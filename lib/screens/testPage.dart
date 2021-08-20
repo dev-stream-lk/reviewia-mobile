7bfd2d6a6febc341d88d3ecb40dcd9e4141b380b
Vikum-Kule <vikumkulatunga@gmail.com>
2021-08-20 12:29:14 +0530

    ad post page updated

--- lib/screens/testPage.dart
+++ lib/screens/testPage.dart
@@ -80,8 +80,7 @@ class _TestAddPostState extends State<TestAddPost> {
   bool checkBox_1_Val = false;
   bool checkBox_2_Val = false;
 
-  int activeStep = 2; // Initial step set to 5.
-
+  int activeStep = 0;
   int upperBound = 2;
   @override
   Widget build(BuildContext context) {
