d3bafb81b3533c4362bad7a1af9a4a158186db8d
Vikum-Kule <vikumkulatunga@gmail.com>
2021-09-08 21:00:05 +0530

    chat list loading created

--- lib/components/groupCard.dart
+++ lib/components/groupCard.dart
@@ -1,128 +1,134 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:reviewia/components/image_box.dart';
 import 'package:reviewia/constrains/constrains.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:reviewia/screens/groupChat.dart';
-import 'package:reviewia/screens/login_system_page.dart';
-import 'package:reviewia/screens/product_view.dart';
-import 'package:reviewia/screens/profile_page.dart';
+import 'package:reviewia/services/userState.dart';
+import 'package:reviewia/structures/chatListStruct.dart';
 
 class GroupCard extends StatefulWidget {
+  late ChatListStruct detail;
+  GroupCard({required this.detail});
+
   @override
   _GroupCardState createState() => _GroupCardState();
 }
 
 class _GroupCardState extends State<GroupCard> {
-  double rate = 3.0;
+  late String email;
+
+  setEmail() async {
+    email = await UserState().getUserName();
+    print("My email::"+ email);
+  }
+
+
+  @override
+  void initState() {
+    setEmail();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, GroupChat.id);
-        },
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.2,
-          margin: EdgeInsets.only(bottom: 10, top: 10),
-          decoration: BoxDecoration(
-            // color: Colors.cyan,
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // changes position of shadow
-              ),
-            ],
-          ),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
+    return GestureDetector(
+      onTap: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => ProductView(todos: widget.detail),
+        //   ),
+        // );
+        // Navigator.pushNamed(context, ProductView.id);
+      },
+      child: Container(
+        // margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 15 / 360,right:MediaQuery.of(context).size.width * 15 / 360,),
+        height: MediaQuery.of(context).size.height * 105 / 659,
+        decoration: BoxDecoration(
+          color: email == widget.detail.createdBy.email?
+          Colors.blue[50]:Colors.amberAccent,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15)),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 15 / 360,
+                  bottom: MediaQuery.of(context).size.width * 15 / 360,
+                  right: MediaQuery.of(context).size.width * 15 / 360,
+                  left: MediaQuery.of(context).size.width * 15 / 360,
+                ),
+                child: DecoratedBox(
                   decoration: BoxDecoration(
-                    color: Color(0xFFFFFEFE),
                     borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5)),
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15)),
+                    color: Colors.white,
                   ),
-
-                  // color:Colors.blueGrey,
-                  child: Row(
+                  child: Image.network("https://cdn.abplive.com/onecms/images/product/fb29564520ae25da9418d044f23db734.jpg?impolicy=abp_cdn&imwidth=300",
+                    fit: BoxFit.cover,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 4,
+                child: Container(
+                  // color: Colors.red,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Expanded(
-                        flex: 1,
-                        child: CircleAvatar(
-                          backgroundColor: Color(0xFFC494C4),
-                          backgroundImage: AssetImage('images/rev_img.png'),
-                          radius: MediaQuery.of(context).size.width * 22.58 / 360,
+                        flex: 2,
+                        child: Center(
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height *
+                                    15 /
+                                    692),
+                            width: double.infinity,
+                            // color: Colors.orange,
+                            child:email == widget.detail.createdBy.email?
+                            Text("Creator: You",
+                              style: KPostCard,
+                            ):Text("Creator: "+
+                              widget.detail.createdBy.firstName+" "+widget.detail.createdBy.lastName,
+                              style: KPostCard,
+                            ),
+
+                          ),
                         ),
                       ),
                       Expanded(
-                          flex: 3,
-                          child:
-                          Text("Chamari Wikrmawardna", style: KPostCard)),
-                      Expanded(
-                          flex: 1,
-                          child:Text(
-                            '(Creator)',
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Colors.black26,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ) ,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  padding:
-                  EdgeInsets.only(left: 15,  right: 15, bottom: 10),
-                  decoration: BoxDecoration(
-                    // color: Color(0xFFAAAAAA),
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(15),
-                        bottomRight: Radius.circular(15)),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
                         flex: 2,
                         child: Container(
+                          // color: Colors.white,
+                          // margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 25/692 ),
                           child: Row(
                             children: [
                               Text(
-                                'Content: ',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  fontFamily: 'Roboto',
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Product 1',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 13,
-                                  fontFamily: 'Roboto',
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              )
+                                  "Members: "+ widget.detail.users.length.toString()),
                             ],
                           ),
                         ),
@@ -130,38 +136,48 @@ class _GroupCardState extends State<GroupCard> {
                       Expanded(
                         flex: 2,
                         child: Container(
-                          child: Row(
-                            children: [
-                              Text(
-                                  'Create Date: ',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  fontFamily: 'Roboto',
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                          Text(
-                            '2021/07/12',
+                          // margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*18/692),
+                          child: Text(
+                            "Created date: " +
+                                widget.detail.createdAt.substring(0, 10),
                             style: TextStyle(
-                              color: Colors.black26,
-                              fontSize: 13,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w500,
+                              fontSize: 10,
                             ),
-                          )
-                            ],
+                            textAlign: TextAlign.left,
                           ),
+                          // color: Colors.yellow,
                         ),
-                      )
+                      ),
                     ],
                   ),
-                ),
-              )
-            ],
-          ),
+                )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                    flex: 2,
+                    child: PopupMenuButton(
+                      icon: Icon(Icons.more_vert),
+                      // onSelected: (item) {
+                      //   selectedOption(
+                      //       item.toString(), widget.detail.postId, context);
+                      //   print(item);
+                      //
+                      // },
+                      itemBuilder: (context) => [
+                        PopupMenuItem(
+                          value: 3,
+                          child: Text(
+                            "Remove",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/chatList.dart
+++ lib/screens/chatList.dart
@@ -3,6 +3,8 @@ import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:reviewia/components/groupCard.dart';
 import 'package:reviewia/components/product_card.dart';
 import 'package:reviewia/constrains/constrains.dart';
+import 'package:reviewia/services/fetchChatList.dart';
+import 'package:reviewia/structures/chatListStruct.dart';
 
 class ChatList extends StatefulWidget {
   static String id = 'ChatList';
@@ -11,7 +13,50 @@ class ChatList extends StatefulWidget {
   _ChatListState createState() => _ChatListState();
 }
 
+
 class _ChatListState extends State<ChatList> {
+  bool _isLoading = true;
+  List<dynamic> _allData = <dynamic>[];
+  List<Users> _users = <Users>[];
+
+
+  _loadChatList() async {
+    var data = await fetchCatList();
+    setState(() {
+      _allData = data;
+      for (var item in _allData) {
+        print('${item.postId} - ${item.id}-${item.createdBy.id}');
+      }
+      print("Lenght of array::"+ _allData.length.toString());
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    setState(() {
+      _isLoading=true;
+    });
+    _loadChatList();
+    super.initState();
+  }
+
+  _listItemViewGroups(int index){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(05),
+          child: GroupCard(detail: _allData[index],),
+        ),
+      ),
+    );
+  }
+
+  Future getBuildingData() async {
+    _loadChatList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,31 +68,41 @@ class _ChatListState extends State<ChatList> {
           ),
         ),
 
-        body: Container(
-          child: SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.fromLTRB(15, 10, 15, 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.015),
-                    child: Text(
-                      'My Chats',
-                      style: KReviewiaMostTitle,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  GroupCard(),
-                  GroupCard(),
-                  GroupCard(),
-                  GroupCard(),
-                  GroupCard(),
-                ],
+        body: RefreshIndicator(
+          onRefresh: getBuildingData,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 10,
               ),
-            ),
+              Text(
+                  "My Chats",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Expanded(
+                flex: 12,
+                // height: MediaQuery.of(context).size.height*0.8,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    if (!_isLoading) {
+                      return _listItemViewGroups(index);
+                      // return _listItem(index);
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                  itemCount: _allData.length,
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+    );
   }
 }
