316b29300e031a0d6b5f52f7559c1e9495844bf1
Vikum-Kule <vikumkulatunga@gmail.com>
2021-09-11 23:56:45 +0530

    chat screen added

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -68,11 +68,38 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
+  Future <void> _SendMessage(String msg) async {
+    String id = widget.detail.id.toString();
+    String token = (await UserState().getToken());
+    String url = KBaseUrl + "api/user/chat?email="+widget.userName+"&group="+id;
+
+    await http.post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Authorization': token,
+        },
+        body: jsonEncode(<String,String>{
+          "content": msg,
+        })
+        ).then((response) {
+      if (response.statusCode==200){
+        _loadChat();
+        print("Message sent");
+      }
+      else{
+        print("Message not sent");
+      }
+    });
+  }
   @override
   void initState() {
     _loadChat();
     super.initState();
     //connectToSocket();
+  }
+
+  Future getRefreshData() async {
+    _loadChat();
   }
 
   @override
@@ -86,29 +113,92 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
 
-      body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: message.length,
-                itemBuilder: (BuildContext context, int index)
-                {
-                  //return Text(message[index].content);
-                  return Row(
+      body: RefreshIndicator(
+        onRefresh: getRefreshData,
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: message.length,
+                  // itemBuilder: (BuildContext context, int index)
+                  // {
+                    //return Text(message[index].content);
+                    // return Row(
+                    //
+                    //   children: [
+                    //     Text(message[index].content)
+                    //   ],
+                    // );
+                  //},
 
-                    children: [
-                      Text(message[index].content)
-                    ],
-                  );
-                },
+                  shrinkWrap: true,
+                  padding: EdgeInsets.only(top: 10,bottom: 10),
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index){
+                    return Container(
+                      padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+                      child: Align(
+                        alignment: (message[index].createdBy == widget.userName?Alignment.topRight:Alignment.topLeft),
+                        child: (message[index].createdBy == widget.userName?
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text("You"+ " - "+ message[index].createdAt.substring(0, 10),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueGrey[100],
+                              ),
 
-              )
-            ),
-            _bottomChatArea()
-          ],
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                  topLeft: Radius.circular(20),
+                                  bottomLeft: Radius.circular(20),
+                                ),
+                                color: (message[index].createdBy == widget.userName?Colors.blue[200]:Colors.grey.shade200),
+                              ),
+                              padding: EdgeInsets.all(16),
+                              child: Text(message[index].content, style: TextStyle(fontSize: 15),),
+                            ),
+                          ],
+                        ):Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(message[index].createdBy+ " - "+ message[index].createdAt.substring(0, 10),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueGrey[100],
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                  bottomLeft: Radius.circular(20),
+                                ),
+                                color: (message[index].createdBy == widget.userName?Colors.blue[200]:Colors.grey.shade200),
+                              ),
+                              padding: EdgeInsets.all(16),
+                              child: Text(message[index].content, style: TextStyle(fontSize: 15),),
+                            ),
+                          ],
+                        )
+                        ),
+                      ),
+                    );
+                  },
+
+                )
+              ),
+              _bottomChatArea()
+            ],
+          ),
         ),
       ),
     );
@@ -126,7 +216,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     print(inputController.text);
 
                     setState(() {
-
+                      _SendMessage(inputController.text);
                     });
                     inputController.text = "";
                   }
